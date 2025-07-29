c99ab36abf2dec518febbbf53ed816be740bc998
Karan Joshi <Karan190213@gmail.com>
2025-07-29 18:39:09 +0530

    Add patient history model and implement related controller methods

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { updateTherapist } = require('../controllers/patientController');
+const { updateTherapist, getPatientHistory, savePatientHistory } = require('../controllers/patientController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
 
 router.patch('/assign-therapist', verifyToken, updateTherapist);
+router.get('/:id/history', verifyToken, getPatientHistory);
+router.post('/:id/history', verifyToken, savePatientHistory);
 
 module.exports = router;
